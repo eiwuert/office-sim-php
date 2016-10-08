6a575424a92442e909bd54f677626e88f7ac3dfd
William Jones <billy@freshjones.com>
2016-10-08 16:01:29 -0400

    working

--- Demos/Demo4/index.php
+++ Demos/Demo4/index.php
@@ -1,6 +1,11 @@
 <?php
 
 require_once(__DIR__ . '/includes/pimple.inc');
+
+require_once(__DIR__ . '/includes/processor.inc');
+
+require_once(__DIR__ . '/includes/simulation.inc');
+
 require_once(__DIR__ . '/includes/service.inc');
 require_once(__DIR__ . '/includes/services.inc');
 require_once(__DIR__ . '/includes/marketing.inc');
@@ -8,16 +13,43 @@ require_once(__DIR__ . '/includes/marketing.inc');
 
 $marketingData = json_decode(file_get_contents(__DIR__ . '/data/marketing.json'),true);
 
-//$container = new Pimple();
 
-//$container['marketing_class'] = 'Marketing';
-//$container['marketing_data'] = $marketingData;
-
-/* we need to get all simulation classes registered */
+/* register all process classes */
+$processContainer = new Pimple();
 foreach($marketingData['services'] AS $key => $data)
 {	
+	if(!$data['active'])
+		continue;
 
+	$processClass = $data['simulation']['classes']['processClass']['value'];
+	$processClassName = 'process_' . strtolower($processClass);
 
+	$processContainer[$processClassName] = function ($c) use ($processClass)
+	{
+	    return new $processClass();
+	};
+
+}
+
+/* register all simulation classes */
+$simulationContainer = new Pimple();
+foreach($marketingData['services'] AS $key => $data)
+{	
+	if(!$data['active'])
+		continue;
+
+	$simClass = $data['simulation']['classes']['simulationClass']['value'];
+	$simClassName = 'simulation_' . strtolower($simClass);
+
+	$processClass = $data['simulation']['classes']['processClass']['value'];
+	$processClassName = 'process_' . strtolower($processClass);
+
+	$processor = $processContainer[$processClassName];
+	
+	$simulationContainer[$simClassName] = function ($c) use ($simClass,$processor)
+	{
+	    return new $simClass($processor);
+	};
 }
 
 $marketingServices = new DepartmentServices();
@@ -26,9 +58,11 @@ foreach($marketingData['services'] AS $key => $data)
 {	
 	if(!$data['active'])
 		continue;
-	
-	$service = new DepartmentService($data);
 
+	$simClass = $data['simulation']['classes']['simulationClass']['value'];
+	$simClassName = 'simulation_' . strtolower($simClass);
+
+	$service = new DepartmentService($data, $simulationContainer[$simClassName]);
 	$marketingServices->addService($service);	
 }
 
